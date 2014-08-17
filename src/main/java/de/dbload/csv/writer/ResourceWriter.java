24a181aef15fa36cf0327822cc0b3ec6c7d10b51
winkler <andre.winkler@web.de>
2014-08-17 13:27:42 +0200

    UPDATE: Round up. Linefeet. To many imports.

--- src/main/java/de/dbload/csv/writer/ResourceWriter.java
+++ src/main/java/de/dbload/csv/writer/ResourceWriter.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2014 Andre Winkler
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -35,16 +35,17 @@ import java.sql.Statement;
 
 import de.dbload.impl.DbloadException;
 
+;
+
 /**
  * Write some SQL results to a <code>.dat</code> file.
- * 
+ *
  * @author Andre Winkler. http://www.andre-winkler.de
  */
 public class ResourceWriter {
 
     private final File file;
     private final Charset utf8;
-    private final String linefeet;
 
     public ResourceWriter(Path _path) {
         this(_path.toFile());
@@ -53,7 +54,6 @@ public class ResourceWriter {
     public ResourceWriter(File _file) {
         file = _file;
         utf8 = Charset.forName("UTF-8");
-        linefeet = System.getProperty("line.separator").toString();
     }
 
     public void start(Connection conn, String sqlSelect, boolean append)
@@ -86,8 +86,7 @@ public class ResourceWriter {
                     ResultSetMetaData metaData = resultSet.getMetaData();
 
                     pw.print("### TAB ");
-                    pw.print(metaData.getTableName(1));
-                    pw.println(linefeet);
+                    pw.println(metaData.getTableName(1));
                     pw.print("### ");
 
                     for (int i = 1; i <= metaData.getColumnCount(); i++) {
@@ -107,6 +106,7 @@ public class ResourceWriter {
                         }
                         pw.println();
                     }
+                    pw.println();
                 }
             }
 
