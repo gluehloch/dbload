42ef0e86aa817efed4a42beb4747be6864a1eaf2
winkler <andre.winkler@web.de>
2014-09-07 00:17:14 +0200

    UPDATE: Use dbload schema for the connection test.

--- src/test/java/de/dbload/jdbc/connector/JdbcMySqlConnectorTest.java
+++ src/test/java/de/dbload/jdbc/connector/JdbcMySqlConnectorTest.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2014 Andre Winkler
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package de.dbload.jdbc.connector;
@@ -28,7 +28,7 @@ import de.dbload.jdbc.connector.JdbcMySqlConnector;
 
 /**
  * Test for {@link JdbcMySqlConnector}.
- * 
+ *
  * @author Andre Winkler. http://www.andre-winkler.de
  */
 public class JdbcMySqlConnectorTest {
@@ -36,7 +36,7 @@ public class JdbcMySqlConnectorTest {
     @Test
     public void testJdbcMySqlConnector() throws Exception {
         Connection connection = JdbcMySqlConnector.createMySqlConnection(
-                "betoffice", "betoffice", "localhost", "betoffice");
+                "dbload", "dbload", "localhost", "dbload");
         assertThat(connection, notNullValue());
 
         connection.rollback();
