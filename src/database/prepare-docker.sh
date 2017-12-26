ccc8bfb6b9cf93fc7e1da53123e69ee190ea858c
Andre Winkler <andre.winkler@web.de>
2017-12-26 12:45:22 +0100

    UPDATE: Moved the docker instance name to a variable.

--- src/database/prepare-docker.sh
+++ src/database/prepare-docker.sh
@@ -1,8 +1,9 @@
 #/bin/bash
+export DOCKER_NAME='dbloaddb'
 export DOCKER_DB_HOST='192.168.99.100'
-docker run --expose=3306 -p 3306:3306 --name dbloaddb -e MYSQL_ROOT_PASSWORD=root -d mariadb:latest
+docker run --expose=3306 -p 3306:3306 --name ${DOCKER_NAME} -e MYSQL_ROOT_PASSWORD=root -d mariadb:latest
 
-export DOCKER_CONTAINER_ID=$(docker inspect -f '{{.Id}}' dbloaddb)
+export DOCKER_CONTAINER_ID=$(docker inspect -f '{{.Id}}' ${DOCKER_NAME})
 CREATE_MARIADB_USER='create_mysql.sql'
 CREATE_MARIADB_SCHEMA='create_mysql_test_schema.sql'
 
