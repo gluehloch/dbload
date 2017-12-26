3ed36cb3b1a5c4f28c4cdc5f47efd3ca8b300299
andrewinkler <andre.winkler@web.de>
2017-12-26 10:29:03 +0100

    Under construction: ... Docker setup ...

--- src/database/prepare-docker.sh
+++ src/database/prepare-docker.sh
@@ -2,8 +2,12 @@
 export DOCKER_DB_HOST='192.168.99.100'
 docker run --expose=3306 -p 3306:3306 --name dbloaddb -e MYSQL_ROOT_PASSWORD=root -d mariadb:latest
 
-export DOCKER_CONTAINER_ID=$(docker inspect -f   '{{.Id}}'  dbloaddb)
-echo $DOCKER_CONTAINER_ID
+export DOCKER_CONTAINER_ID=$(docker inspect -f '{{.Id}}' dbloaddb)
+CREATE_MARIADB_USER='create_mysql.sql'
+CREATE_MARIADB_SCHEMA='create_mysql_test_schema.sql'
+
+docker cp ${CREATE_MARIADB_USER} ${DOCKER_CONTAINER_ID}:/tmp/${CREATE_MARIADB_USER}
+docker cp ${CREATE_MARIADB_SCHEMA} ${DOCKER_CONTAINER_ID}:/tmp/${CREATE_MARIADB_SCHEMA}
 
 #mysql -u root --password=root -h $DOCKER_DB_HOST < create_mysql.sql
 #mysql -u root --password=root -h $DOCKER_DB_HOST < create_mysql_test_schema.sql
