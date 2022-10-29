7667068bd734f2bdc50bc5a59ac7d7fd50e136fb
gluehloch <gluehloch@googlemail.com>
2022-10-29 20:23:17 +0200

    Under construction: Adding command line options.

--- src/main/java/de/dbload/DbLoadMain.java
+++ src/main/java/de/dbload/DbLoadMain.java
@@ -1,9 +1,21 @@
 package de.dbload;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class DbLoadMain {
 
     public static void main(String[] args) {
-        System.out.println("Das ist ein Test.");
+        Options options = CommandLineParserConfig.create();
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine cmd = parser.parse(options, args);
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
     }
 
 }
