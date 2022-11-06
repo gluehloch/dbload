737ee1f9c3710b6d3a5831993759f749232ef1be
gluehloch <gluehloch@googlemail.com>
2022-11-06 13:16:55 +0100

    Export example: The full betoffice database.

--- src/test/java/de/dbload/DbloadCommandLineTest.java
+++ src/test/java/de/dbload/DbloadCommandLineTest.java
@@ -1,8 +1,6 @@
-package de.dbload.csv;
+package de.dbload;
 
 import org.junit.jupiter.api.Test;
-
-import de.dbload.DbloadMain;
 
 class DbloadCommandLineTest {
 
@@ -11,7 +9,7 @@ class DbloadCommandLineTest {
         // -u betoffice -p betoffice -d jdbc:mariadb://127.0.0.1/betoffice --tables bo_session -f export.dat --export
         final String[] args = {"-u", "betoffice", "-p", "betoffice",
                 "-d", "jdbc:mariadb://127.0.0.1/betoffice",
-                "--tables", "bo_session,bo_user",
+                "--tables", "bo_session,bo_user,bo_team,bo_teamalias,bo_grouptype,bo_season,bo_group,bo_team_group,bo_player,bo_location,bo_gamelist,bo_game,bo_goal,bo_gametipp,bo_community,bo_community_user",
                 "-f", "export.dat",
                 "--export"};
         DbloadMain.main(args);
